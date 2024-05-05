4d50aaa62bfb1da2b73703c3d62ace079c7caa8b
rimlklola <rimlakhiri22@gmail.com>
2024-05-05 20:24:59 +0100

    firstcommit

---
+++ controllers/app.js
@@ -0,0 +1,45 @@
+const express = require('express'); /*kanimportiw express*/
+
+const app = express(); /*kancreayiw app */ 
+const PORT = 3000; 
+
+app.get('/', (req, res)=>{ 
+    res.status(200); 
+    res.send("Welcome to root URL of Server"); 
+});
+
+app.listen(PORT, (error) =>{ 
+	if(!error) 
+		console.log("Server is Successfully Running, and App is listening on port "+ PORT) 
+	else
+		console.log("Error occurred, server can't start", error); 
+	} 
+); 
+
+// Require all controller files
+const blogPostController = require('./controllers/blogpostcontroller');
+const categoryController = require('./controllers/categorycontroller');
+const commentController = require('./controllers/commentcontroller');
+
+
+// Define routes for blog post controller
+app.post('/api/posts', blogpostcontroller.createPost);
+app.get('/api/posts', blogpostcontroller.getAllPosts);
+app.get('/api/posts/category/:categoryId', blogpostcontroller.getPostsByCategory);
+app.put('/api/posts/:postId', blogpostcontroller.updatePost);
+app.delete('/api/posts/:postId', blogpostcontroller.deletePost);
+
+// Define routes for category controller
+app.post('/api/categories', categorycontroller.createCategory);
+app.get('/api/categories', categorycontroller.getAllCategories);
+app.get('/api/categories/:categoryId', categorycontroller.getCategoryById);
+app.put('/api/categories/:categoryId', categorycontroller.updateCategory);
+app.delete('/api/categories/:categoryId', categorycontroller.deleteCategory);
+
+// Define routes for comment controller
+app.post('/api/comments', commentcontroller.createComment);
+app.get('/api/comments', commentcontroller.getAllComments);
+app.get('/api/comments/:commentId', commentcontroller.getCommentById);
+app.put('/api/comments/:commentId', commentcontroller.updateComment);
+app.delete('/api/comments/:commentId', commentcontroller.deleteComment);
+
